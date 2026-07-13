fc2bbe517c0e367c569315fa0569d78ba0665f34
Krishna <krishnagupta2531699@gmail.com>
2026-07-13 11:35:22 +0530

    Time: 2 ms (47.87%), Space: 49 MB (32.94%) - LeetHub - Time: O(n + m), Space: O(n + m)

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,36 +1,41 @@
 class Solution {
     public double findMedianSortedArrays(int[] num1, int[] num2) {
-           int[] arr= new int[num1.length+num2.length];
-           int i=0,j=0,k=0;
-           while(i<num1.length && j<num2.length){
-               if(num1[i]<num2[j]){
-                  arr[k++]=num1[i++];
-               }
-               else{
-                   arr[k++]=num2[j++];
-               }
-               
-           }
-           for(;i<num1.length;i++){
-             arr[k++]=num1[i];
-           }
-           for(;j<num2.length;j++){
-            arr[k++]=num2[j];
-           }
 
-           int n=arr.length;
-           
-           if(n%2!=0){
-            int  midIndex=n/2;
-              return arr[midIndex];
-           }
-           else{
-              double mid1=arr[n/2-1];
-              double mid2=arr[n/2];
-              double median=(mid1+mid2)/2;
-              return median;
-           }
-           
+        int[] arr = new int[num1.length + num2.length];
+
+        int i = 0, j = 0, k = 0;
+
+        while(i < num1.length && j < num2.length){
+            if(num1[i] < num2[j]){
+                arr[k++] = num1[i++];
+            }
+            else{
+                arr[k++] = num2[j++];
+            }
+        }
+
+        while(i < num1.length){
+            arr[k] = num1[i];
+            i++;
+            k++;
+        }
+
+        while(j < num2.length){
+            arr[k] = num2[j];
+            j++;
+            k++;
+        }
+
+        int n = arr.length;
+
+        if(n % 2 != 0){
+            return arr[n/2];
+        }
+        else{
+            double mid1 = arr[n/2 - 1];
+            double mid2 = arr[n/2];
+            return (mid1 + mid2) / 2.0;
+        }
     }
 }
 
