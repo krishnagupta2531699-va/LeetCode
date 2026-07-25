6e03fa435673a65146702fc60a3acb5cd275a1b9
Krishna <krishnagupta2531699@gmail.com>
2026-07-25 19:54:08 +0530

    Time: 1 ms (99.96%), Space: 42.5 MB (67.86%) - LeetHub - Time: O(log n), Space: O(1)

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.java
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.java
@@ -1,22 +1,46 @@
 class Solution {
     public long sumAndMultiply(int n) {
-        String s=Integer.toString(n);
-        char[] arr=s.toCharArray();
-        int store=0;
+        long x=0;
         int sum=0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]=='0'){
-                continue;
+        while(n!=0){
+            int d=n%10;
+            if(d!=0){
+                x=x*10+d;
+                sum+=d;
             }
-            else{
-                int c=arr[i]-'0';
-                sum+=c;
-                store= store * 10 + c;
-            }
+            n=n/10;
         }
-        return (long)sum*store;
+         long rev = 0;
+        while (x!=0) {
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+        return rev*sum;
     }
 }
+
+
+
+
+// class Solution {
+//     public long sumAndMultiply(int n) {
+//         String s=Integer.toString(n);
+//         char[] arr=s.toCharArray();
+//         int store=0;
+//         int sum=0;
+//         for(int i=0;i<arr.length;i++){
+//             if(arr[i]=='0'){
+//                 continue;
+//             }
+//             else{
+//                 int c=arr[i]-'0';
+//                 sum+=c;
+//                 store= store * 10 + c;
+//             }
+//         }
+//         return (long)sum*store;
+//     }
+// }
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
