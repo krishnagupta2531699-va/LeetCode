78576ef8015af3e83256e0aa9ffe23ddd95f9c14
Krishna <krishnagupta2531699@gmail.com>
2026-07-28 23:13:55 +0530

    Time: 0 ms (100%), Space: 43.3 MB (36.22%) - LeetHub - Time: O(stones.length() * jewels.length()), Space: O(1)

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -1,16 +1,54 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-        HashSet<Character> set=new HashSet<>();
         int count=0;
-        for(int i=0;i<jewels.length();i++){
-            set.add(jewels.charAt(i));
-        }
         for(int i=0;i<stones.length();i++){
-            if(set.contains(stones.charAt(i))) count++;
+            for(int j=0;j<jewels.length();j++){
+                if(stones.charAt(i)==jewels.charAt(j)){
+                    count++;
+                    break;
+                }
+            }
         }
         return count;
     }
 }
+
+
+// class Solution {
+//     public int numJewelsInStones(String jewels, String stones) {
+//         int[] freq=new int[256];
+//         int count=0;
+//         for(int i=0;i<jewels.length();i++){
+//             freq[jewels.charAt(i)]=1;
+//         }
+//         for(int i=0;i<stones.length();i++){
+//            if(freq[stones.charAt(i)]==1){
+//             count++;
+//            }
+           
+//         }
+//         return count;
+//     }
+// }
+
+
+
+// class Solution {
+//     public int numJewelsInStones(String jewels, String stones) {
+//         HashSet<Character> set=new HashSet<>();
+//         int count=0;
+//         for(int i=0;i<jewels.length();i++){
+//             set.add(jewels.charAt(i));
+//         }
+//         for(int i=0;i<stones.length();i++){
+//             if(set.contains(stones.charAt(i))) count++;
+//         }
+//         return count;
+//     }
+// }
+
+
+
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
