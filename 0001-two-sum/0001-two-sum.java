d58988c2d92134dafd55eb56c9e7ce305887e36c
Krishna <krishnagupta2531699@gmail.com>
2026-07-28 21:36:49 +0530

    Time: 2 ms (99.29%), Space: 47.3 MB (23.44%) - LeetHub - Time: O(n), Space: O(n)

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,17 +1,37 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] s=new int[2];
+        int[] c=new int[2];
+        HashMap<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target){
-                         s[0]=i;
-                         s[1]=j;
-                }
-            }
+             int need=target-nums[i];
+             if(map.containsKey(need)){
+                c[0]=map.get(need);
+                c[1]=i;
+                return c;
+             }
+             map.put(nums[i],i);
         }
-        return s;
+        return c;
     }
 }
+
+
+
+
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         int[] s=new int[2];
+//         for(int i=0;i<nums.length;i++){
+//             for(int j=i+1;j<nums.length;j++){
+//                 if(nums[i]+nums[j]==target){
+//                          s[0]=i;
+//                          s[1]=j;
+//                 }
+//             }
+//         }
+//         return s;
+//     }
+// }
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
