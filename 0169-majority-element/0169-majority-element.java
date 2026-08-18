0e725ab431c354261bc01981baa4fc99205d4c73
Krishna <krishnagupta2531699@gmail.com>
2026-08-18 15:16:49 +0530

    Time: 8 ms (40.51%), Space: 55.3 MB (70.27%) - LeetHub - Time: O(n log n), Space: O(log n)

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,28 +1,67 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer,Integer> map=new HashMap<>();
+        Arrays.sort(nums);
         int n=nums.length;
-        for(int i=0;i<n;i++){
-            if(map.containsKey(nums[i])){
-               map.put(nums[i], map.get(nums[i]) + 1);
-            }
-            else{
-                map.put(nums[i],1);
-            }
-        }
-       
-       int max = 0;
-        int answer = 0;
+        int max=n/2;
+        int count=1;
+        int i=0,j=i+1;
+        int ans=nums[0];
+        while(j<n){
+           if(nums[i]==nums[j]){
+              count++;
+               j++;
 
-        for (int ele : map.keySet()) {
-            if (map.get(ele) > max) {
-                max = map.get(ele);
-                answer = ele;
-            }
+           }
+           else{
+              
+              if(count>max){
+                 ans=nums[i];
+                 return ans;
+              }
+              i=j;
+              count=1;
+              j++;
+           }
+
         }
-        return answer;
+        if(count>max){
+            ans=nums[i];
+        }
+
+        return ans;
+
+           
     }
 }
+
+
+
+
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         HashMap<Integer,Integer> map=new HashMap<>();
+//         int n=nums.length;
+//         for(int i=0;i<n;i++){
+//             if(map.containsKey(nums[i])){
+//                map.put(nums[i], map.get(nums[i]) + 1);
+//             }
+//             else{
+//                 map.put(nums[i],1);
+//             }
+//         }
+       
+//        int max = 0;
+//         int answer = 0;
+
+//         for (int ele : map.keySet()) {
+//             if (map.get(ele) > max) {
+//                 max = map.get(ele);
+//                 answer = ele;
+//             }
+//         }
+//         return answer;
+//     }
+// }
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
