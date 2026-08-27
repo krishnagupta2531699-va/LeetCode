c17bec94911f014444b60adcfa785e7633f5642e
Krishna <krishnagupta2531699@gmail.com>
2026-08-27 16:10:23 +0530

    Time: 0 ms (100%), Space: 42.8 MB (72.25%) - LeetHub - Time: O(n), Space: O(1)

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -10,24 +10,38 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
-        if(head.next==null) return head;
-        ListNode temp=head;
-
-        int size=0;
-        while(temp!=null){
-            temp=temp.next;
-            size++;
+       ListNode slow=head;
+       ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
         }
-        int idx=size/2;
-        temp=head;
-        for(int i=1;i<=idx-1;i++){
-            temp=temp.next;
-        }
-        temp=temp.next;
-        return temp;
-        
+        return slow;
     }
 }
+
+
+
+// class Solution {
+//     public ListNode middleNode(ListNode head) {
+//         if(head.next==null) return head;
+//         ListNode temp=head;
+
+//         int size=0;
+//         while(temp!=null){
+//             temp=temp.next;
+//             size++;
+//         }
+//         int idx=size/2;
+//         temp=head;
+//         for(int i=1;i<=idx-1;i++){
+//             temp=temp.next;
+//         }
+//         temp=temp.next;
+//         return temp;
+        
+//     }
+// }
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
