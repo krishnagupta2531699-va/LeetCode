53265554e1f8a60ed3a5ae6f1bfbe8088723e8e0
Krishna <krishnagupta2531699@gmail.com>
2026-08-27 16:38:25 +0530

    Time: 3 ms (99.93%), Space: 202.5 MB (62.09%) - LeetHub - Time: O(n), Space: O(1)

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
@@ -11,26 +11,86 @@
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
         if (head.next == null) return null;
-
-        ListNode temp = head;
-        int size = 0;
-
-        while (temp != null) {
-            temp = temp.next;
-            size++;
+        ListNode small=head;
+        ListNode fast=head.next.next;
+        while(fast!=null && fast.next!=null){
+            small=small.next;
+            fast=fast.next.next;
         }
-
-        temp = head;
-
-        for (int i = 1; i <= size / 2 - 1; i++) {
-            temp = temp.next;
-        }
-
-        temp.next = temp.next.next;
-
+        small.next=small.next.next;
         return head;
     }
 }
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public ListNode deleteMiddle(ListNode head) {
+//         if (head.next == null) return null;
+//         ListNode small=head;
+//         ListNode fast=head;
+//         while(fast.next!=null && fast.next.next!=null){
+//             small=small.next;
+//             fast=fast.next.next;
+//         }
+//         small.next=small.next.next;
+//         return head;
+//     }
+// }
+
+
+
+
+
+// class Solution {
+//     public ListNode deleteMiddle(ListNode head) {
+//          if (head.next == null) return null;
+//         ListNode small=head;
+//         ListNode fast=head;
+//         while(fast!=null && fast.next!=null){
+//             small=small.next;
+//             fast=fast.next.next;
+//         }
+//         ListNode temp=head;
+//         while(temp.next!=small){
+//             temp=temp.next;
+//         }
+//         temp.next=temp.next.next;
+//         return head;
+//     }
+// }
+
+
+
+// class Solution {
+//     public ListNode deleteMiddle(ListNode head) {
+//         if (head.next == null) return null;
+
+//         ListNode temp = head;
+//         int size = 0;
+
+//         while (temp != null) {
+//             temp = temp.next;
+//             size++;
+//         }
+
+//         temp = head;
+
+//         for (int i = 1; i <= size / 2 - 1; i++) {
+//             temp = temp.next;
+//         }
+
+//         temp.next = temp.next.next;
+
+//         return head;
+//     }
+// }
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
