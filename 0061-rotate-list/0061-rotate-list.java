1f0210efa323e4200488d8a1e91aeb8efc17df84
Krishna <krishnagupta2531699@gmail.com>
2026-08-29 14:47:57 +0530

    Time: 0 ms (100%), Space: 43.9 MB (96.05%) - LeetHub - Time: O(n), Space: O(1)

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -1,13 +1,5 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+
+
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
         if (head == null || head.next == null) return head;
@@ -22,26 +14,70 @@ class Solution {
         k=k%size;
         if(k==0) return head;
         
-        temp=head;
-        for(int i=1;i<(size-k);i++){
+        ListNode slow=head;
+        ListNode fast=head;
+        for(int i=1;i<=k+1;i++){
+            fast=fast.next;
+        }
+        while(fast!=null){
+            slow=slow.next;
+            fast=fast.next;
+        }
+        ListNode a=slow.next;
+        slow.next=null;
+        temp=a;
+        while(temp.next!=null){
             temp=temp.next;
-
         }
-        ListNode a=temp.next;
-        temp.next=null;
-        ListNode last = a;
-
-        while (last.next != null) {
-            last = last.next;
-        }
-
-        last.next = head;
-        head = a;
-        return head;
+        temp.next=head;
+        head=a;
+        return a;
 
 
     }
 }
+
+
+
+
+
+
+
+
+// class Solution {
+//     public ListNode rotateRight(ListNode head, int k) {
+//         if (head == null || head.next == null) return head;
+//         ListNode temp=head;
+
+//         int size=0;
+//         while(temp!=null){
+//             temp=temp.next;
+//             size++;
+//         }
+       
+//         k=k%size;
+//         if(k==0) return head;
+        
+//         temp=head;
+//         for(int i=1;i<(size-k);i++){
+//             temp=temp.next;
+
+//         }
+//         ListNode a=temp.next;
+//         temp.next=null;
+//         ListNode last = a;
+
+//         while (last.next != null) {
+//             last = last.next;
+//         }
+
+//         last.next = head;
+//         head = a;
+//         return head;
+
+
+//     }
+// }
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
