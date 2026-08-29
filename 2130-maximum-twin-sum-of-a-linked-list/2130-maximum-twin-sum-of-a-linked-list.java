9a8eb1a624b2ca0a4e38f8d01ff6db2a1cc366dc
Krishna <krishnagupta2531699@gmail.com>
2026-08-29 21:46:27 +0530

    Time: 5 ms (56.91%), Space: 101.6 MB (40.34%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
@@ -0,0 +1,52 @@
+
+class Solution {
+    public int pairSum(ListNode head) {
+        ListNode dum1=new ListNode(-1);
+        ListNode t=dum1;
+        ListNode dum2=new ListNode(-2);
+        ListNode z=dum2;
+        ListNode i=head;
+        int l=0;
+        while(i!=null){
+             if(l%2==0){
+                t.next=i;
+                t=i;
+             }
+             else{
+                z.next=i;
+                z=i;
+             }
+             l++;
+             i=i.next;
+        }
+        t.next=null;
+        z.next=null;
+        ListNode a=dum1.next;
+        ListNode b=dum2.next;
+        ListNode prev=null;
+        ListNode curr=b;
+        ListNode next=curr.next;
+        while(curr!=null){
+         next=curr.next;
+         curr.next=prev;
+         prev=curr;
+         curr=next;
+      }
+        b=prev;
+        ListNode j=b;
+        ListNode k=a;
+        int max=Integer.MIN_VALUE;
+        while(j!=null){
+            if(k.val+j.val>max){
+                max=k.val+j.val;
+            }
+            k=k.next;
+            j=j.next;
+        }
+        return max;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
