127b8e17cabe3cbb753841bd2ff19aad1424f1d5
Krishna <krishnagupta2531699@gmail.com>
2026-08-29 19:52:10 +0530

    Time: 0 ms (100%), Space: 44.6 MB (13.98%) - LeetHub - Time: O(n), Space: O(1)

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -1,44 +1,48 @@
-
 class Solution {
     public ListNode reverseList(ListNode head) {
-        if(head==null || head.next==null) return head;
-        ArrayList<ListNode> arr=new ArrayList<>();
-        ListNode temp=head;
-        while(temp!=null){
-            arr.add(temp);
-            temp=temp.next;
-        }
-        int n=arr.size();
-        for(int i=n-1;i>=1;i--){
-            arr.get(i).next=arr.get(i-1);
-        }
-        arr.get(0).next=null;
-        return arr.get(n-1);
+      if(head==null) return null;
+      ListNode prev=null;
+      ListNode curr=head;
+      ListNode next=curr.next;
+      while(curr!=null){
+         next=curr.next;
+         curr.next=prev;
+         prev=curr;
+         curr=next;
+      }
+      return prev;
+      
+      
+
     }
 }
 
 
 
 
-
 // class Solution {
 //     public ListNode reverseList(ListNode head) {
-//       if(head==null) return null;
-//       ListNode prev=null;
-//       ListNode curr=head;
-//       ListNode next=curr.next;
-//       while(curr!=null){
-//          next=curr.next;
-//          curr.next=prev;
-//          prev=curr;
-//          curr=next;
-//       }
-//       return prev;
-      
-      
-
+//         if(head==null || head.next==null) return head;
+//         ArrayList<ListNode> arr=new ArrayList<>();
+//         ListNode temp=head;
+//         while(temp!=null){
+//             arr.add(temp);
+//             temp=temp.next;
+//         }
+//         int n=arr.size();
+//         for(int i=n-1;i>=1;i--){
+//             arr.get(i).next=arr.get(i-1);
+//         }
+//         arr.get(0).next=null;
+//         return arr.get(n-1);
 //     }
 // }
+
+
+
+
+
+
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
