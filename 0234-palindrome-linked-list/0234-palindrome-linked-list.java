38901ebd058257a7901f023dfa1246de7115e9b3
Krishna <krishnagupta2531699@gmail.com>
2026-08-29 21:17:59 +0530

    Time: 4 ms (67.23%), Space: 94.2 MB (79.64%) - LeetHub - Time: O(n), Space: O(1)

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -1,21 +1,83 @@
 
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        ArrayList<Integer> arr=new ArrayList<>();
-        ListNode temp=head;
-        while(temp!=null){
-            arr.add(temp.val);
-            temp=temp.next;
+
+        if (head == null) return false;
+        if (head.next == null) return true;
+
+        ListNode temp = head;
+        int size = 0;
+
+        while (temp != null) {
+            size++;
+            temp = temp.next;
         }
-        int i=0,j=arr.size()-1;
-        while(i<j){
-            if(arr.get(i)!=arr.get(j)) return false;
-            i++;
-            j--;
+
+        int s = size / 2;
+        temp = head;
+
+      
+        for (int i = 0; i < s - 1; i++) {
+            temp = temp.next;
         }
+
+        ListNode a;
+
+      
+        if (size % 2 != 0) {
+            a = temp.next.next;
+        } else {
+            a = temp.next;
+        }
+
+       
+        ListNode curr = a;
+        ListNode prev = null;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        a = prev;
+
+      
+        ListNode p = head;
+        ListNode q = a;
+
+        while (q != null) {
+            if (p.val != q.val) {
+                return false;
+            }
+
+            p = p.next;
+            q = q.next;
+        }
+
         return true;
     }
 }
+
+
+// class Solution {
+//     public boolean isPalindrome(ListNode head) {
+//         ArrayList<Integer> arr=new ArrayList<>();
+//         ListNode temp=head;
+//         while(temp!=null){
+//             arr.add(temp.val);
+//             temp=temp.next;
+//         }
+//         int i=0,j=arr.size()-1;
+//         while(i<j){
+//             if(arr.get(i)!=arr.get(j)) return false;
+//             i++;
+//             j--;
+//         }
+//         return true;
+//     }
+// }
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
