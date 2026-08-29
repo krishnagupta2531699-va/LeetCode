d45c1622e45d1403e2332a33d0eb97cd7c82940d
Krishna <krishnagupta2531699@gmail.com>
2026-08-29 20:33:59 +0530

    Time: 7 ms (27.62%), Space: 99.8 MB (15.02%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -0,0 +1,22 @@
+
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ArrayList<Integer> arr=new ArrayList<>();
+        ListNode temp=head;
+        while(temp!=null){
+            arr.add(temp.val);
+            temp=temp.next;
+        }
+        int i=0,j=arr.size()-1;
+        while(i<j){
+            if(arr.get(i)!=arr.get(j)) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
