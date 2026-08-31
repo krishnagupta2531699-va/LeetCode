7cdd13cdc2efbf2afb39239017a097e98a15d217
Krishna <krishnagupta2531699@gmail.com>
2026-08-31 15:50:46 +0530

    Time: 2 ms (73.65%), Space: 43 MB (83.6%) - LeetHub - Time: O(n), Space: O(1)

--- 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.java
+++ 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.java
@@ -19,6 +19,41 @@ class Solution {
     }
 }
 
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public boolean areOccurrencesEqual(String s) {
+//         HashMap<Character,Integer> map=new HashMap<>();
+//         for(int i=0;i<s.length();i++){
+//             if(map.containsKey(s.charAt(i))){
+//                 map.put(s.charAt(i),map.get(s.charAt(i))+1);
+//             }
+//             else{
+//                 map.put(s.charAt(i),1);
+//             }
+//         }
+//        int count = map.get(s.charAt(0));
+//         for(char ele :map.keySet()){
+//             if(count!=map.get(ele)){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
+
+
+
+
+
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
 // Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
