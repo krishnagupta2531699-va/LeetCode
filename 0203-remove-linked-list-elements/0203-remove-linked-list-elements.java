9e770ea4bb93dfce0b15d6b1e62fa5a2be1a3b1f
Krishna <krishnagupta2531699@gmail.com>
2026-09-01 12:11:49 +0530

    Time: 1 ms (96.91%), Space: 47.3 MB (59.96%) - LeetHub - Time: O(N), Space: O(1)

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
@@ -1,37 +1,50 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
-        while(head!=null){
-            if(head.val==val){
-                head=head.next;
-            }
-            else{
-                break;
-            }
-        }
-        ListNode temp=head;
+        ListNode dum=new ListNode(-1);
+        ListNode t=dum;
        
-        while(temp!=null && temp.next!=null){
-            if(temp.next.val==val){
-                temp.next=temp.next.next;
+        ListNode temp=head;
+        while(temp!=null){
+            if(temp.val!=val){
+                t.next=temp;
+                t=temp;
             }
-            else{
-                temp=temp.next;
-            }
-           
+            temp=temp.next;
         }
-        return head;
+        t.next=null;
+        return dum.next;
+
     }
 }
+
+
+
+
+// class Solution {
+//     public ListNode removeElements(ListNode head, int val) {
+//         while(head!=null){
+//             if(head.val==val){
+//                 head=head.next;
+//             }
+//             else{
+//                 break;
+//             }
+//         }
+//         ListNode temp=head;
+       
+//         while(temp!=null && temp.next!=null){
+//             if(temp.next.val==val){
+//                 temp.next=temp.next.next;
+//             }
+//             else{
+//                 temp=temp.next;
+//             }
+           
+//         }
+//         return head;
+//     }
+// }
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
