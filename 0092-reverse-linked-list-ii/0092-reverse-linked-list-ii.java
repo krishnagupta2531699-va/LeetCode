08f95fa23e4d5a49d2f0bd8359fab05066ff7fb3
Krishna <krishnagupta2531699@gmail.com>
2026-09-01 21:13:28 +0530

    Time: 0 ms (100%), Space: 43.1 MB (29.35%) - LeetHub - Time: O(n), Space: O(1)

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
@@ -1,78 +1,57 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        ArrayList<ListNode> arr=new ArrayList<>();
-        ListNode temp=head;
-        while(temp!=null){
-            arr.add(temp);
-            temp=temp.next;
+
+        if (left == right) {
+            return head;
         }
-        int i=left-1;
-        int j=right-1;
-        while(i<=j){
-            ListNode a=arr.get(i);
-            ListNode b=arr.get(j);
-            ListNode t=a;
-            arr.set(i,b);
-            arr.set(j,t);
-            i++;
-            j--;
-        }
-        for(i=0;i<arr.size();i++){
-            if(i==arr.size()-1){
-                arr.get(i).next=null;
+
+        if (left == 1) {
+           
+            ListNode prev = null;
+            ListNode curr = head;
+
+            for (int i = 1; i <= right; i++) {
+                ListNode f = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = f;
             }
-            else{
-            arr.get(i).next=arr.get(i+1);}
+
+            head.next = curr;
+            return prev;
         }
-        return arr.get(0);
+
+        ListNode temp = head;
+
+        for (int i = 1; i < left - 1; i++) {
+            temp = temp.next;
+        }
+
+        ListNode t = temp.next;
+        ListNode k = t;
+
+        for (int i = left; i < right; i++) {
+            k = k.next;
+        }
+
+        ListNode c = k.next;
+
+        ListNode prev = null;
+        ListNode curr = t;
+
+        while (curr != c) {
+            ListNode f = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = f;
+        }
+
+        temp.next = prev;
+        t.next = c;
+
+        return head;
     }
 }
-
-
-
-// class Solution {
-//     public ListNode reverseBetween(ListNode head, int left, int right) {
-//         if(head==null || head.next==null) return head;
-//         ListNode t=head;
-//         for(int i=0;i<left-2;i++){
-//             t=t.next;
-
-//         }
-//         ListNode before = t;
-//         ListNode a = t.next;
-        
-      
-//         t=head;
-//         for(int i=0;i<right-1;i++){
-//             t=t.next;
-//         }
-//         ListNode b=t;
-//         ListNode after = b.next;
-//         ListNode prev=after;
-//         ListNode curr=a;
-//         while (curr != after) {
-//             ListNode next = curr.next;
-//             curr.next = prev;
-//             prev = curr;
-//             curr = next;
-//         }
-//         before.next=prev;
-//         return head;
-
-
-
-//     }
-// }
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
