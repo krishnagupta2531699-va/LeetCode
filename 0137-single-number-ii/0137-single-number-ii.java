21a1891dd98cb6e6581093b24d7d97c02627ccb4
Krishna <krishnagupta2531699@gmail.com>
2026-09-07 12:06:54 +0530

    Time: 6 ms (44.28%), Space: 46.5 MB (16.64%) - LeetHub - Time: O(n), Space: O(n)

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,16 +1,41 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length-1;i+=3){
-            if(nums[i]!=nums[i+1]){
-                return nums[i];
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i]) + 1);
+            } else {
+                map.put(nums[i], 1);
             }
         }
-        return nums[nums.length-1];
-        
+
+        for (int ele : map.keySet()) {
+            if (map.get(ele) == 1) {
+                return ele;
+            }
+        }
+
+        return 0;
     }
-   
 }
+
+
+
+
+// class Solution {
+//     public int singleNumber(int[] nums) {
+//         Arrays.sort(nums);
+//         for(int i=0;i<nums.length-1;i+=3){
+//             if(nums[i]!=nums[i+1]){
+//                 return nums[i];
+//             }
+//         }
+//         return nums[nums.length-1];
+        
+//     }
+   
+// }
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
