224cc7a2ced4d330425d592a356fa1ec6e99dd94
Krishna <krishnagupta2531699@gmail.com>
2026-09-09 12:17:47 +0530

    Time: 6 ms (6.6%), Space: 43.3 MB (92.38%) - LeetHub - Time: O(n^2), Space: O(n)

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        Arrays.sort(arr);
+        ArrayList<Integer> x=new ArrayList<>();
+        int i=0;
+        int j=i+1;
+        int count=1;
+        while(j<arr.length){
+            if(arr[i]==arr[j]){
+                count++;
+
+            }
+            else{
+                x.add(count);
+                count=1;
+                i=j;
+            }
+            j++;
+        }
+        x.add(count);
+        for (i = 0; i < x.size(); i++) {
+            for (j = i + 1; j < x.size(); j++) {
+
+                if (x.get(i).equals(x.get(j))) {
+                    return false;
+                }
+            }
+        }
+        return true;
+       
+
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
