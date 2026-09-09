08ec28c438de6624ededc8ef54ebb0c5720df222
Krishna <krishnagupta2531699@gmail.com>
2026-09-09 13:14:20 +0530

    Time: 3 ms (35.67%), Space: 43.5 MB (69.6%) - LeetHub - Time: O(n), Space: O(n)

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -1,36 +1,61 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-        Arrays.sort(arr);
-        ArrayList<Integer> x=new ArrayList<>();
-        int i=0;
-        int j=i+1;
-        int count=1;
-        while(j<arr.length){
-            if(arr[i]==arr[j]){
-                count++;
-
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            if(map.containsKey(arr[i])){
+                map.put(arr[i],map.get(arr[i])+1);
             }
-            else{
-                x.add(count);
-                count=1;
-                i=j;
-            }
-            j++;
+           else map.put(arr[i],1);
         }
-        x.add(count);
-        for (i = 0; i < x.size(); i++) {
-            for (j = i + 1; j < x.size(); j++) {
-
-                if (x.get(i).equals(x.get(j))) {
-                    return false;
-                }
-            }
-        }
-        return true;
-       
-
+       HashSet<Integer> set=new HashSet<>();
+       for(int ele:map.keySet()){
+          if(!set.contains(map.get(ele))){
+            set.add(map.get(ele));
+          }
+          else{
+            return false;
+          }
+       }
+       return true;
     }
 }
+
+
+
+
+// class Solution {
+//     public boolean uniqueOccurrences(int[] arr) {
+//         Arrays.sort(arr);
+//         ArrayList<Integer> x=new ArrayList<>();
+//         int i=0;
+//         int j=i+1;
+//         int count=1;
+//         while(j<arr.length){
+//             if(arr[i]==arr[j]){
+//                 count++;
+
+//             }
+//             else{
+//                 x.add(count);
+//                 count=1;
+//                 i=j;
+//             }
+//             j++;
+//         }
+//         x.add(count);
+//         for (i = 0; i < x.size(); i++) {
+//             for (j = i + 1; j < x.size(); j++) {
+
+//                 if (x.get(i).equals(x.get(j))) {
+//                     return false;
+//                 }
+//             }
+//         }
+//         return true;
+       
+
+//     }
+// }
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
