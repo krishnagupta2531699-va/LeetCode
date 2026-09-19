98c8011dd16eb5f49c6a03426ca54e0a9a3e64b7
Krishna <krishnagupta2531699@gmail.com>
2026-09-19 21:04:38 +0530

    Added solution - Time: O(n), Space: O(n)

--- 701343-next-greater-element-GFG/701343-next-greater-element.java
+++ 701343-next-greater-element-GFG/701343-next-greater-element.java
@@ -1,48 +1,43 @@
 class Solution {
     public ArrayList<Integer> nextLargerElement(int[] arr) {
-        // code here
-        ArrayList<Integer> x= new ArrayList<>();
-        Stack<Integer> stack=new Stack<>();
-        int n=arr.length;
-        stack.add(arr[n-1]);
+        ArrayList<Integer> x = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
+
+        int n = arr.length;
+
+        stack.push(arr[n - 1]);
         x.add(-1);
-        int i=n-2;
-        while(i>=0){
-            if(arr[i]<stack.peek()){
+
+        int i = n - 2;
+
+        while (i >= 0) {
+
+            while (stack.size() > 0 && stack.peek() <= arr[i]) {
+                stack.pop();
+            }
+
+            if (stack.size() == 0) {
+                x.add(-1);
+            } else {
                 x.add(stack.peek());
-                stack.push(arr[i]);
-                i--;
             }
-            else if(arr[i]>=stack.peek()){
-                while(stack.size()>0 && arr[i]>=stack.peek()){
-                    stack.pop();
-                }
-                if(stack.size()>0){
-                    x.add(stack.peek());
-                }
-                else{
-                    x.add(-1);
-                }
-                stack.push(arr[i]);
-                i--;
-            }
+
+            stack.push(arr[i]);
+            i--;
         }
 
+        int j = 0;
+        int k = x.size() - 1;
 
-       int j = 0;
-       int k = x.size() - 1;
+        while (j < k) {
+            int temp = x.get(j);
+            x.set(j, x.get(k));
+            x.set(k, temp);
+            j++;
+            k--;
+        }
 
-       while (j < k) {
-           int temp = x.get(j);
-           x.set(j, x.get(k));
-           x.set(k, temp);
-
-           j++;
-           k--;
-       }
-
-       return x;
-
+        return x;
     }
 }
 
