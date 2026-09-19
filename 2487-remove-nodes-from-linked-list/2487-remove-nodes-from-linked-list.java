764e58fd3c700be8445dc97c165925c18be3cb1a
Krishna <krishnagupta2531699@gmail.com>
2026-09-19 20:07:31 +0530

    Time: 6 ms (92.63%), Space: 130.6 MB (64.08%) - LeetHub - Time: O(n), Space: O(1)

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
@@ -1,52 +1,117 @@
 class Solution {
     public ListNode removeNodes(ListNode head) {
-        Stack<ListNode> stack = new Stack<>();
-        ListNode temp = head;
 
-        while (temp != null) {
-            if (stack.size() == 0) {
-                stack.push(temp);
-                temp = temp.next;
+         head=reverseList(head);
+         ListNode i=head;
+         ListNode j=head.next;
+         while(j!=null){
+            if(i.val>j.val){
+                j=j.next;
             }
-            else if (stack.peek().val < temp.val) {
-                while (stack.size() > 0 && temp.val > stack.peek().val) {
-                    stack.pop();
-                }
-                stack.push(temp);
-                temp = temp.next;
+            else{
+                i.next=j;
+                i=j;
+                j=j.next;
             }
-            else {
-                stack.push(temp);
-                temp = temp.next;
-            }
-        }
-
-        ArrayList<ListNode> arr = new ArrayList<>();
-
-        while (stack.size() > 0) {
-            arr.add(stack.pop());
-        }
-
-        int i = 0;
-        int j = arr.size() - 1;
-
-        while (i < j) {
-            int value = arr.get(i).val;
-            arr.get(i).val = arr.get(j).val;
-            arr.get(j).val = value;
-            i++;
-            j--;
-        }
-
-        for (i = 0; i < arr.size() - 1; i++) {
-            arr.get(i).next = arr.get(i + 1);
-        }
-
-        arr.get(arr.size() - 1).next = null;
-
-        return arr.get(0);
+         }
+         i.next=null;
+         head=reverseList(head);
+         return head;
     }
-}
+    public ListNode reverseList(ListNode head) {
+      if(head==null) return null;
+      ListNode prev=null;
+      ListNode curr=head;
+      ListNode next=curr.next;
+      while(curr!=null){
+         next=curr.next;
+         curr.next=prev;
+         prev=curr;
+         curr=next;
+      }
+      return prev;
+      
+      
+
+    }
+      
+      
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public ListNode removeNodes(ListNode head) {
+//         Stack<ListNode> stack = new Stack<>();
+//         ListNode temp = head;
+
+//         while (temp != null) {
+//             if (stack.size() == 0) {
+//                 stack.push(temp);
+//                 temp = temp.next;
+//             }
+//             else if (stack.peek().val < temp.val) {
+//                 while (stack.size() > 0 && temp.val > stack.peek().val) {
+//                     stack.pop();
+//                 }
+//                 stack.push(temp);
+//                 temp = temp.next;
+//             }
+//             else {
+//                 stack.push(temp);
+//                 temp = temp.next;
+//             }
+//         }
+
+//         ArrayList<ListNode> arr = new ArrayList<>();
+
+//         while (stack.size() > 0) {
+//             arr.add(stack.pop());
+//         }
+
+//         int i = 0;
+//         int j = arr.size() - 1;
+
+//         while (i < j) {
+//             int value = arr.get(i).val;
+//             arr.get(i).val = arr.get(j).val;
+//             arr.get(j).val = value;
+//             i++;
+//             j--;
+//         }
+
+//         for (i = 0; i < arr.size() - 1; i++) {
+//             arr.get(i).next = arr.get(i + 1);
+//         }
+
+//         arr.get(arr.size() - 1).next = null;
+
+//         return arr.get(0);
+//     }
+// }
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
